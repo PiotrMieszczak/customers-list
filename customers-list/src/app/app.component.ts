d3d39c063b44b4e225eea2ff3285fe717f95d089
PiotrMieszczak_adar <piotr.mieszczak@adar.pl>
2019-02-02 14:53:48 +0100

    refactor(AppComponent): Code clean up
    Code clean up

--- customers-list/src/app/app.component.ts
+++ customers-list/src/app/app.component.ts
@@ -1,22 +1,4 @@
 import { Component } from '@angular/core';
-import * as faker from 'faker';
-
-class smallCustomer {
-  // id: string;
-  type: number
-  name: string;
-  country: string;
-  websiteUrl: string;
-  numberOfEmployees: number;
-  contractExpiryDate: string;
-
-  constructor(data) {
-    Object.keys(data).forEach(key => {
-      this['key'] = data['key'];
-    })
-  }
-}
-
 
 @Component({
   selector: 'app-root',
