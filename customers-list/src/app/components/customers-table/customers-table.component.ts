234c5c5115768093e29181a72e9147c2db7a21b0
PiotrMieszczak_adar <piotr.mieszczak@adar.pl>
2019-02-02 18:55:55 +0100

    feat(customers-table.component): Development of table component
    Added new methods for getting data from server - includes sorting and
    pagination

--- customers-list/src/app/components/customers-table/customers-table.component.ts
+++ customers-list/src/app/components/customers-table/customers-table.component.ts
@@ -1,22 +1,109 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Observable, Subject, combineLatest, of } from 'rxjs';
+import { map, takeUntil, startWith, catchError,  switchMap, } from 'rxjs/operators';
+
+import { MatPaginator, MatSort } from '@angular/material';
+
+import { CustomersTableService } from './customers-table.service';
 import { ToolBarData } from './../../classes/toolbarData';
-import { Component, OnInit } from '@angular/core';
+import { QueryParams } from '../../classes/queryParams';
+import { Customer } from '../../classes/customer';
 
 @Component({
   selector: 'customers-table',
   templateUrl: './customers-table.component.html',
   styleUrls: ['./customers-table.component.scss']
 })
-export class CustomersTableComponent implements OnInit {
-  public toolBarData: ToolBarData;
+export class CustomersTableComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  public toolBarData: ToolBarData;
+  public dataSource$: Observable<Customer[]>;
+  public displayedColumns: string[] = [
+    'type', 
+    'name', 
+    'country', 
+    'website', 
+    'numberOfEmployees', 
+    'contractExpiryDate',
+    'annualTurnover',
+    'complianceChecked'
+  ];
+  private _guard$ = new Subject();
+  constructor(private _customersTableService: CustomersTableService) { }
 
   ngOnInit() {
     this.createToolBarData();
   }
 
+  ngAfterViewInit() {
+    this.getCustomersData();
+  }
 
-  createToolBarData() {
+  /**
+   * Sets toolbar data
+   * 
+   * @returns void
+   */
+  createToolBarData(): void {
     this.toolBarData = new ToolBarData('Customers List', 'primary');
+  }
+
+  /**
+   * Gets customers data based on pagination and sort data
+   * 
+   * @returns void
+   */
+  getCustomersData(): void {
+    this.dataSource$ = combineLatest(this.sort.sortChange.pipe(startWith({})), this.paginator.page.pipe(startWith({})))
+      .pipe(
+        // takeUntil(this._guard$),
+        switchMap(([sortData, paginationData]) => {
+          const params = this.createQueryParams(sortData, paginationData);
+          return this._customersTableService.getCustomersList(params)
+            .pipe(
+              map(customers => {
+                return customers.map(customer => {
+                  customer.contractExpiryDate = this._customersTableService.formatDate(customer.contractExpiryDate);
+                  return customer;
+                })
+              }),
+              catchError(err => {
+                // TO DO Error handling
+                console.error(err);
+                return of([]);
+              })
+            )
+        })
+      )
+  }
+
+  /**
+   * Creates query params based on sort and pagination data
+   * 
+   * @param  {} sortData
+   * @param  {} pagData
+   * @returns QueryParams
+   */
+  createQueryParams(sortData, pagData): QueryParams {
+    const params = new QueryParams();
+    params.sortBy(sortData.active, sortData.direction);
+    params.limit(pagData.pageSize);
+    return params;
+  }
+
+  /**
+   * Creates columns names
+   * 
+   * @param  {string} columnName
+   * @returns string
+   */
+  createDisplayedColumnName(columnName: string): string {
+    return columnName.split(/(?=[A-Z])/).join(' ').toLocaleUpperCase();
+  }
+
+  ngOnDestroy(): void {
+    this._guard$.next();
   }
 }
--- customers-list/src/app/components/customers-table/customers-table.service.ts
+++ customers-list/src/app/components/customers-table/customers-table.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+import { Customer } from '../../classes/customer';
+import { QueryParams } from '../../classes/queryParams';
+
+import { HttpService } from '../../http.service'
+import * as moment from 'moment';
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersTableService {
 
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
 
-  
+  /**
+   * Formats date string to MM/DD/YYYY format
+   * 
+   * @param  {string} date
+   * @returns string
+   */
+  formatDate(date: string): string {
+    return moment(date).format('MM/DD/YYYY');
+  }
+
+  /**
+   * Gets all customers from data base
+   * 
+   * @param  {QueryParams} params
+   * @returns Observable
+   */
+  getCustomersList(params: QueryParams): Observable<Customer[]> {
+    const queryParams = this._httpService.toQueryString(params);
+    return this._httpService.get<Customer[]>('customer?'+ queryParams);
+  }
 }
