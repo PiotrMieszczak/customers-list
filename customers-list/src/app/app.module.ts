3731a097e0974377cd0c94b14fe75bd670ae802a
PiotrMieszczak_adar <piotr.mieszczak@adar.pl>
2019-02-02 18:52:01 +0100

    feat: Addded momentjs + style paths in package-lock.json
    Addded momentjs + style paths in package-lock.json

--- customers-list/src/app/app.module.ts
+++ customers-list/src/app/app.module.ts
@@ -1,29 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppSettings } from './app.settings';
 
+import { AppComponent } from './app.component';
 import { CustomersTableComponent } from './components/customers-table/customers-table.component';
 import { CustomersTableService } from './components/customers-table/customers-table.service';
 import { ToolbarComponent } from './components/shared/toolbar/toolbar.component';
+import { HttpService } from './http.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomersTableComponent,
-    ToolbarComponent
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
-    CustomersTableService
+    AppSettings,
+    CustomersTableService,
+    HttpService,
   ],
   bootstrap: [AppComponent]
 })
--- customers-list/src/app/app.settings.ts
+++ customers-list/src/app/app.settings.ts
@@ -3,5 +3,5 @@ import { environment } from '../environments/environment';
 export class AppSettings {
   public protocol = 'http://'
   public apiIp = this.protocol + environment.location;
-  public apiUrl = this.apiIp + '/api';
+  public apiUrl = this.apiIp + '/';
 }
